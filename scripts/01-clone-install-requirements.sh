63ba41ef580355a7900587ea10d66dbf060225bf
Ubuntu <ubuntu@ip-172-31-42-5.ec2.internal>
2024-10-30 23:08:59 +0000

    first-commit

---
+++ scripts/01-clone-install-requirements.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+cd /home/ubuntu
+
+read -p "Enter your GitHub username: " github_username
+
+# Prompt user for Personal Access Token
+read -s -p "Enter your GitHub Personal Access Token: " github_pat
+
+echo ""
+echo "GitHub Username: $github_username"
+echo "Personal Access Token: [Hidden]"
+
+echo ""
+echo "=================="
+echo "Cloning repo"
+echo "=================="
+echo ""
+
+
+git clone https://$github_username:$github_pat@github.com/JohnYanxinLiu/IDL_Group56_Voice_Privacy_Challenge.git
+echo $?
+# if [[ $?!=0 ]];then
+#     echo "Issue cloning repo"
+#     exit -1
+# fi
+echo ""
+echo "======================="
+echo "installing dependencies"
+echo "======================="
+echo ""
+
+cd ./IDL_Group56_Voice_Privacy_Challenge/
+chmod +x ./00_install.sh 
+./00_install.sh
+source env.sh
+
+echo ""
+echo "======================="
+echo "Success!!"
+echo "======================="
+
---
+++ scripts/02-download-data.sh
@@ -0,0 +1,9 @@
+cd /home/ubuntu/IDL_Group56_Voice_Privacy_Challenge
+
+echo ""
+echo "======================="
+echo "Downloading data"
+echo "======================="
+echo ""
+chmod +x ./01_download_data_model.sh
+./01_download_data_model.sh
