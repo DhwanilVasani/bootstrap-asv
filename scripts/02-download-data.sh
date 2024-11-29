f37200957ee74c5f7e3a2b9315f7d1e192026c45
Ubuntu <ubuntu@ip-172-31-42-5.ec2.internal>
2024-11-29 07:50:11 +0000

    Added commands to download the anon data

--- scripts/02-download-data.sh
+++ scripts/02-download-data.sh
@@ -2,8 +2,22 @@ cd /home/ubuntu/Voice-Privacy-Challenge-2024
 
 echo ""
 echo "======================="
-echo "Downloading data"
+echo "Downloading data - Repo script"
 echo "======================="
 echo ""
 chmod +x ./01_download_data_model.sh
 ./01_download_data_model.sh
+
+
+echo ""
+echo "======================="
+echo "To download ALLL the anonymized data, please run the following command, make sure you have enough space on your disk"
+echo "wget -O "Attacker.zip"  "https://www.dropbox.com/scl/fo/et019wjcfx2ngyr8eriv3/AOv3RmA6lRT6BfM8WQ5Lezw?e=4&preview=B3.tar.gz&rlkey=4npadiy3r82mbpt91zj327prr&st=9eshrm31&dl=1"
+--2024-11-29 06:02:20--  https://www.dropbox.com/scl/fo/et019wjcfx2ngyr8eriv3/AOv3RmA6lRT6BfM8WQ5Lezw?e=4&preview=B3.tar.gz&rlkey=4npadiy3r82mbpt91zj327prr&st=9eshrm31&dl=1"
+echo "======================="
+
+echo ""
+echo "======================="
+echo "To check if you another volume that you can use, run the following command"
+echo "lsblk"
+echo "======================="
